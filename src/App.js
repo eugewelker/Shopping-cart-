9f9c35f79407ffbf3f9e1a1df15abce265fbcde2
eugewelker <eugewelker88@gmail.com>
2019-12-01 22:00:46 +0000

    first commit shopping cart repo

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {Container} from 'react-bootstrap';
+import SubTotal from './components/Subtotal/Subtotal';
+import './App.css';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: 100
+    };
+  }
+
+
+  render() {
+    return (
+      <div className="container" >
+        <Container className="purchase-card">
+        <SubTotal price={this.state.total.toFixed(2)}  />
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default App;
