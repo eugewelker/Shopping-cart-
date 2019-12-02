50bc11c99a06bba7611397edf1496e4589bc1514
eugewelker <eugewelker88@gmail.com>
2019-12-02 14:59:24 +0000

    added estimated total

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Container} from 'react-bootstrap';
 import SubTotal from './components/Subtotal/Subtotal';
 import PickupSavings from './components/PickupSavings/PickupSavings';
 import TaxesFees from './components/TaxesFees/TaxesFees';
+import EstimatedTotal from './components/EstimatedTotal/EstimatedTotal';
 import './App.css';
 
 
@@ -13,7 +14,8 @@ class App extends Component {
     this.state = {
       total: 100,
       PickupSavings:-3.85,
-      taxes:0
+      taxes:0,
+      estimatedTotal:0
     };
   }
 
@@ -25,6 +27,8 @@ class App extends Component {
         <SubTotal price={this.state.total.toFixed(2)}  />
         <PickupSavings price={this.state.PickupSavings} />
         <TaxesFees taxes={this.state.taxes.toFixed(2)} />
+        <hr />
+        <EstimatedTotal price={this.state.estimatedTotal.toFixed(2)} />
         </Container>
       </div>
     );
